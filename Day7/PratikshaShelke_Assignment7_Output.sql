4ca8adbf146897e7ab03050ebfed1dd87cd98057
Pratiksha Shelke <pratiksha.shelke2012@gmail.com>
2025-05-01 14:04:01 -0400

    Add files via upload

--- Day7/PratikshaShelke_Assignment7_Output.sql
+++ Day7/PratikshaShelke_Assignment7_Output.sql
@@ -1,69 +1,88 @@
 
-1. Categorize products by stock status
-(Display product_name, a new column stock_status whose values are based on below condition
- units_in_stock = 0  is 'Out of Stock'
-       units_in_stock < 20  is 'Low Stock')
+Assignment  7
+
+1.     Rank employees by their total sales
+(Total sales = Total no of orders handled, JOIN employees and orders table)
+
 
 SELECT 
-    product_name,
-    CASE 
-        WHEN units_in_stock = 0 THEN 'Out of Stock'
-        WHEN units_in_stock < 20 THEN 'Low Stock'
-        ELSE 'In Stock'
-    END AS stock_status
-FROM products;
------------------------------------------------------------------------------------------------------------------ 
-2. Find All Products in Beverages Category
-(Subquery, Display product_name,unitprice)
+    e.employee_id,
+    e.first_name || e.last_name AS employeename,
+    COUNT(o.order_id) AS total_sales,
+    RANK() OVER (ORDER BY COUNT(o.order_id) DESC) AS sales_rank
+FROM 
+    employees e
+JOIN 
+    orders o ON e.employee_id = o.employee_id
+GROUP BY 
+    e.employee_id, employeename
+ORDER BY 
+    total_sales DESC;
 
-SELECT product_name, unit_price
-FROM products
-WHERE category_id = (
-    SELECT category_id
-    FROM categories
-    WHERE category_name = 'Beverages'
-);
+-- Using Dense rank
+SELECT e.employee_id,
+       COUNT(o.order_id) AS total_sales,
+       DENSE_RANK() OVER (ORDER BY COUNT(o.order_id) DESC) AS employee_sales_rank
+FROM Employees e
+LEFT JOIN Orders o ON e.employee_id = o.employee_id
+GROUP BY e.employee_id
+ORDER BY employee_sales_rank
 
------------------------------------------------------------------------------------------------------------------
-3. Find Orders by Employee with Most Sales
-(Display order_id,   order_date,  freight, employee_id.
-Employee with Most Sales=Get the total no.of of orders for each employee then order by DESC and limit 1. Use Subquery)
+-- Rank over
+SELECT e.employee_id,
+       COUNT(o.order_id) AS total_sales,
+       RANK() OVER (ORDER BY COUNT(o.order_id) DESC) AS employee_sales_rank
+FROM Employees e
+LEFT JOIN Orders o ON e.employee_id = o.employee_id
+GROUP BY e.employee_id
+ORDER BY employee_sales_rank
+---------------------------------------------------------------------------------------------------
+2.Compare current order's freight with previous and next order for each customer.
+(Display order_id,  customer_id,  order_date,  freight,
+Use lead(freight) and lag(freight).
 
-SELECT order_id, order_date, freight, employee_id
-FROM orders
-WHERE employee_id = (
-    SELECT employee_id
-    FROM orders
-    GROUP BY employee_id
-    ORDER BY COUNT(order_id) DESC
-    LIMIT 1
-);
+SELECT 
+    order_id,
+    customer_id,
+    order_date,
+    freight,
+    LAG(freight) OVER (PARTITION BY customer_id ORDER BY order_date) AS previous_freight,
+    LEAD(freight) OVER (PARTITION BY customer_id ORDER BY order_date) AS next_freight
+FROM 
+    orders;
 
------------------------------------------------------------------------------------------------------------------
- 
-4. Find orders  where for country!= ‘USA’ with freight costs higher than any order from USA. (Subquery, Try with ANY, ALL operators)
+--------------------------------------------------------------------------------------------------
+3.     Show products and their price categories, product count in each category, avg price:
+        	(HINT:
+·  	Create a CTE which should have price_category definition:
+        	WHEN unit_price < 20 THEN 'Low Price'
+            WHEN unit_price < 50 THEN 'Medium Price'
+            ELSE 'High Price'
+·  	In the main query display: price_category,  product_count in each price_category,  ROUND(AVG(unit_price)::numeric, 2) as avg_price)
 
-1) Using ANY
+WITH price_categories AS (
+    SELECT 
+        product_name,
+        unit_price,
+        CASE 
+            WHEN unit_price < 20 THEN 'Low Price'
+            WHEN unit_price < 50 THEN 'Medium Price'
+            ELSE 'High Price'
+        END AS price_category
+    FROM 
+        products
+)
+SELECT 
+    price_category,
+    COUNT(*) AS product_count,
+    ROUND(AVG(unit_price)::numeric, 2) AS avg_price
+FROM 
+    price_categories
+GROUP BY 
+    price_category
+ORDER BY 
+    price_category;
 
-SELECT order_id, order_date, freight, ship_country
-FROM orders
-WHERE ship_country != 'USA'
-  AND freight > ANY (
-      SELECT freight
-      FROM orders
-      WHERE ship_country = 'USA'
-  );
 
 
-2)Using ALL
 
-SELECT order_id, order_date, freight, ship_country
-FROM orders
-WHERE ship_country != 'USA'
-  AND freight > ALL (
-      SELECT freight
-      FROM orders
-      WHERE ship_country = 'USA'
-  );
- 
------------------------------------------------------------------------------------------------------------------
